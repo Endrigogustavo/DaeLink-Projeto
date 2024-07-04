a124bfad003ceb34258c572eaed248660afd69e0
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-04 16:21:05 -0300

    Atualização da home

--- Src/Apresentation/Web/src/App.jsx
+++ Src/Apresentation/Web/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
 function App() {
   return (
     <>
       <div className="app">
         <Navbar />
+        <Home/>
       </div>
 
     </>
---
+++ Src/Apresentation/Web/src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+import Def from './Img/Home.png'
+
+
+function Home() {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Home
