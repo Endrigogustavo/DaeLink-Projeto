296c378fab594f46996b113965c05125ab4bc5dc
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-04 17:52:56 -0300

    Atualização da home

--- Src/Apresentation/Web/src/Components/Home/Home.jsx
+++ Src/Apresentation/Web/src/Components/Home/Home.jsx
@@ -6,6 +6,25 @@ function Home() {
 
     return (
         <>
+<main class="main">
+   <section class="section banner banner-section">
+      <div class="container banner-column">
+         <img class="banner-image" src={Def} alt="banner"/>
+         <div class="banner-inner">
+            <h1 class="heading-xl">DaeLink a melhor plafatorma de trabalho</h1>
+            <p class="paragraph">
+               Uma plataforma de empregabilidade com foco em empresas contratarem pessoas portadoras de deficiencia
+            </p>
+         </div>
+         <div class="banner-links">
+            <a href="#" title=""><i class="bx bxl-facebook"></i></a>
+            <a href="#" title=""><i class="bx bxl-instagram"></i></a>
+            <a href="#" title=""><i class="bx bxl-twitter"></i></a>
+            <a href="#" title=""><i class="bx bxl-youtube"></i></a>
+         </div>
+      </div>
+   </section>
+</main>
         </>
     )
 }
