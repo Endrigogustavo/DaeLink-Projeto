de1f8501b5a120e29472f3d4c7bb8937f3f904ea
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-05 09:40:22 -0300

    Atualização home

---
+++ Src/Apresentation/Web/src/Components/Home/Card.jsx
@@ -0,0 +1,57 @@
+import './Home.css'
+
+function Card() {
+
+    return (
+        <>
+     <section>
+	<div class="customer-feedback">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-sm-offset-2 col-sm-8">
+					<div>
+						<h2 class="section-title">Empresas parceiras</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class=" body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-64 overflow-hidden">
+          <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503"/>
+        </div>
+        <h2 class="text-xl font-medium title-font text-white mt-5">IBM</h2>
+        <p class="text-base leading-relaxed mt-2">Swag shoindxgoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+       
+      </div>
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-64 overflow-hidden">
+          <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504"/>
+        </div>
+        <h2 class="text-xl font-medium title-font text-white mt-5">Adaplay</h2>
+        <p class="text-base leading-relaxed mt-2">Swag shoindxigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+      
+      </div>
+      <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div class="rounded-lg h-64 overflow-hidden">
+          <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505"/>
+        </div>
+        <h2 class="text-xl font-medium title-font text-white mt-5">Aftable Toys</h2>
+        <p class="text-base leading-relaxed mt-2">Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+        
+      </div>
+    </div>
+  </div>
+</section>
+
+        </>
+    )
+}
+
+export default Card
--- Src/Apresentation/Web/src/Components/Home/Home.jsx
+++ Src/Apresentation/Web/src/Components/Home/Home.jsx
@@ -1,60 +1,41 @@
 import './Home.css'
+import './Card.css'
+
+import Card from './Card'
 import Def from './Img/HomeImg.png'
 
 
 function Home() {
 
-    return (
-        <>
-<main class="main">
-   <section class="section banner banner-section">
-      <div class="container banner-column">
-         <img class="banner-image" src={Def} alt="banner"/>
-         <div class="banner-inner">
-            <h1 class="heading-xl">DaeLink a melhor plataforma de trabalho</h1>
-            <p class="paragraph">
-               Uma plataforma de empregabilidade com foco em empresas contratarem pessoas portadoras de deficiencia
-            </p>
-         </div>
-         <div class="banner-links">
-            <a href="#" title=""><i class="bx bxl-facebook"></i></a>
-            <a href="#" title=""><i class="bx bxl-instagram"></i></a>
-            <a href="#" title=""><i class="bx bxl-twitter"></i></a>
-            <a href="#" title=""><i class="bx bxl-youtube"></i></a>
-         </div>
-      </div>
-   </section>
-</main>
+  return (
+    <>
+      <main class="main">
+        <section class="section banner banner-section">
+          <div class="container banner-column">
+            <img class="banner-image" src={Def} alt="banner" />
+            <div class="banner-inner">
+              <h1 class="heading-xl">DaeLink a melhor plataforma de trabalho</h1>
+              <p class="paragraph">
+                Uma plataforma de empregabilidade com foco em empresas contratarem pessoas portadoras de deficiencia
+              </p>
+            </div>
+            <div class="banner-links">
+              <a href="#" title=""><i class="bx bxl-facebook"></i></a>
+              <a href="#" title=""><i class="bx bxl-instagram"></i></a>
+              <a href="#" title=""><i class="bx bxl-twitter"></i></a>
+              <a href="#" title=""><i class="bx bxl-youtube"></i></a>
+            </div>
+          </div>
+        </section>
 
+        <div className="card-home">
+          <Card />
+        </div>
 
-  <div class="row1-container">
-    <div class="box box-down cyan">
-      <h2>Supervisor</h2>
-      <p>Monitors activity to identify project roadblocks</p>
-      <img src="https://assets.codepen.io/2301174/icon-supervisor.svg" alt=""/>
-    </div>
+      </main>
 
-    <div class="box red">
-      <h2>Team Builder</h2>
-      <p>Scans our talent network to create the optimal team for your project</p>
-      <img src="https://assets.codepen.io/2301174/icon-team-builder.svg" alt=""/>
-    </div>
-
-    <div class="box box-down blue">
-      <h2>Calculator</h2>
-      <p>Uses data from past projects to provide better delivery estimates</p>
-      <img src="https://assets.codepen.io/2301174/icon-calculator.svg" alt=""/>
-    </div>
-  </div>
-  <div class="row2-container">
-    <div class="box orange">
-      <h2>Karma</h2>
-      <p>Regularly evaluates our talent to ensure quality</p>
-      <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt=""/>
-    </div>
-  </div>
-        </>
-    )
+    </>
+  )
 }
 
 export default Home
