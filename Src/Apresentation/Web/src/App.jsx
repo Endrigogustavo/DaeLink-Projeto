3e41390ae346f9d1c6fc7c4e0b3e1ea5cfa2e894
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-05 11:43:51 -0300

    Criação da pagina de vagas

--- Src/Apresentation/Web/src/App.jsx
+++ Src/Apresentation/Web/src/App.jsx
@@ -1,14 +1,20 @@
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Footer from './Components/Footer/Footer'
+import Vagas from './Components/Vagas/Vagas'
+
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div className="app">
-        <Navbar />
-       <Home/>
-       <Footer/>
-      </div>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vagas" element={<Vagas />} />
+      </Routes>
+      <Footer />
+
 
     </>
   )
