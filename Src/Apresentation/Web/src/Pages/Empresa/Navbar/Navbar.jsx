f58b970d8e53c78ffdc2d0f63a7abaeb9f8f249c
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-10 23:31:17 -0300

    Criação da tela de visualizar processos empresa

--- Src/Apresentation/Web/src/Pages/Empresa/Navbar/Navbar.jsx
+++ Src/Apresentation/Web/src/Pages/Empresa/Navbar/Navbar.jsx
@@ -36,6 +36,10 @@ function Navbar() {
         navigate(`/candidatos/${IdEmpresa}`);
     };
 
+    const handleButtonClickVaga = (IdEmpresa) => {
+        navigate(`/processos/${IdEmpresa}`);
+    };
+
     return (
         <>
             <header>
@@ -50,12 +54,12 @@ function Navbar() {
                                     <ul class="space-y-6 tracking-wide font-medium text-base lg:text-sm items-center justify-center lg:flex lg:space-y-0">
                                         <li>
                                             <center>
-                                                <Link  to="/" class="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
+                                                <button onClick={() => handleButtonClickVaga(userId)}  class="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
                                                     <svg data-name="Layer 2" width="25px" height="25px" id="Layer_2" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                                         <path class="cls-1" d="M30,15a1,1,0,0,1-.58-.19L16,5.23,2.58,14.81a1,1,0,0,1-1.16-1.62l14-10a1,1,0,0,1,1.16,0l14,10A1,1,0,0,1,30,15Z" /><path class="cls-1" d="M5,9A1,1,0,0,1,4,8V4A1,1,0,0,1,5,3H9A1,1,0,0,1,9,5H6V8A1,1,0,0,1,5,9Z" /><path class="cls-1" d="M25,29H20a1,1,0,0,1-1-1V21H13v7a1,1,0,0,1-1,1H7a3,3,0,0,1-3-3V16a1,1,0,0,1,2,0V26a1,1,0,0,0,1,1h4V20a1,1,0,0,1,1-1h8a1,1,0,0,1,1,1v7h4a1,1,0,0,0,1-1V16a1,1,0,0,1,2,0V26A3,3,0,0,1,25,29Z" />
                                                     </svg>
-                                                    <span>Home</span>
-                                                </Link>
+                                                    <span>Meus Processos</span>
+                                                </button>
                                             </center>
                                         </li>
                                         <li>
--- Src/Apresentation/Web/src/Pages/PCD/Vagas/Enviar_documentos.jsx
+++ Src/Apresentation/Web/src/Pages/PCD/Vagas/Enviar_documentos.jsx
@@ -9,6 +9,41 @@ const AddDocumentoForm = () => {
   const { id, vagaId } = useParams();
   const [userId, setUserId] = useState(id);
   const [vagaUid, setVagaUid] = useState(vagaId);
+
+  const [nome, setNome] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [telefone, setTel] = useState("");
+  const [email, setEmail] = useState("");
+  const [idade, setIdade] = useState("");
+
+  const [objetivo, setObjetivo] = useState("");
+
+  const [experiencia1, setExp1] = useState("");
+  const [experiencia2, setExp2] = useState("");
+  const [experiencia3, setExp3] = useState("");
+
+  const [formacao1a, setForm1a] = useState("");
+  const [formacao2a, setForm2a] = useState("");
+  const [formacao3a, setForm3a] = useState("");
+
+  const [formacao1c, setForm1c] = useState("");
+  const [formacao2c, setForm2c] = useState("");
+  const [formacao3c, setForm3c] = useState("");
+
+
+  const [qualificacao1, setQuali1] = useState("");
+  const [qualificacao2, setQuali2] = useState("");
+  const [qualificacao3, setQuali3] = useState("");
+  const [qualificacao4, setQuali4] = useState("");
+  const [qualificacao5, setQuali5] = useState("");
+
+  const [idiomas1, setIdioma1] = useState("");
+  const [idiomas2, setIdioma2] = useState("");
+
+  const [informatica, setInfo] = useState("");
+
+  
+
   const [documento, setDocumento] = useState(null);
 
   useEffect(() => {
@@ -20,11 +55,6 @@ const AddDocumentoForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (!vagaUid || !userId || !documento) {
-      alert("Por favor, preencha todos os campos.");
-      return;
-    }
 
     try {
 
@@ -47,6 +77,29 @@ const AddDocumentoForm = () => {
         const documentosRef = collection(candidatoDocRef, "documentos");
 
         await addDoc(documentosRef, {
+          nome: nome,
+          endereco: endereco,
+          telefone: telefone,
+          email: email,
+          idade: idade,
+          objetivo: objetivo,
+          experiencias1: experiencia1,
+          experiencias2: experiencia2,
+          experiencias3: experiencia3,
+          formacao_academica1: formacao1a,
+          formacao_academica2: formacao2a,
+          formacao_academica3: formacao3a,
+          formacao_complementar1: formacao1c,
+          formacao_complementar2: formacao2c,
+          formacao_complementar3: formacao3c,
+          qualificação1: qualificacao1,
+          qualificação2: qualificacao2,
+          qualificação3: qualificacao3,
+          qualificação4: qualificacao4,
+          qualificação5: qualificacao5,
+          idioma1: idiomas1,
+          idioma2: idiomas2,
+          informatica: informatica,
           url: downloadURL,
           userId: userId  
         });
@@ -73,6 +126,237 @@ const AddDocumentoForm = () => {
           onChange={(e) => setUserId(e.target.value)}
         />
       </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Telefone"
+          value={telefone}
+          onChange={(e) => setTel(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Endereço"
+          value={endereco}
+          onChange={(e) => setEndereco(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Idade"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        />
+      </div>
+<br />
+<br />
+<div>
+        <input
+          type="text"
+          placeholder="Objetivo Profissional"
+          value={objetivo}
+          onChange={(e) => setObjetivo(e.target.value)}
+        />
+      </div>
+<br />
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Experiencia 1"
+          value={experiencia1}
+          onChange={(e) => setExp1(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Experiencia 2"
+          value={experiencia2}
+          onChange={(e) => setExp2(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Experiencia 3"
+          value={experiencia3}
+          onChange={(e) => setExp3(e.target.value)}
+        />
+      </div>
+<br />
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Formação academica 1"
+          value={formacao1a}
+          onChange={(e) => setForm1a(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Formação academica 2"
+          value={formacao2a}
+          onChange={(e) => setForm2a(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Formação academica 3"
+          value={formacao3a}
+          onChange={(e) => setForm3a(e.target.value)}
+        />
+      </div>
+<br />
+<br />
+<div>
+        <input
+          type="text"
+          placeholder="Formação complementar 1"
+          value={formacao1c}
+          onChange={(e) => setForm1c(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Formação complementar 2"
+          value={formacao2c}
+          onChange={(e) => setForm2c(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Formação complementar 3"
+          value={formacao3c}
+          onChange={(e) => setForm3c(e.target.value)}
+        />
+      </div>
+<br />
+<br />
+<div>
+        <input
+          type="text"
+          placeholder="Qualificação 1"
+          value={qualificacao1}
+          onChange={(e) => setQuali1(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Qualificação 2"
+          value={qualificacao2}
+          onChange={(e) => setQuali2(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Qualificação 3"
+          value={qualificacao3}
+          onChange={(e) => setQuali3(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Qualificação 4"
+          value={qualificacao4}
+          onChange={(e) => setQuali4(e.target.value)}
+        />
+      </div>
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Qualificação 5"
+          value={qualificacao5}
+          onChange={(e) => setQuali5(e.target.value)}
+        />
+      </div>
+<br />
+<br />
+
+<div>
+        <input
+          type="text"
+          placeholder="Idioma 1"
+          value={idiomas1}
+          onChange={(e) => setIdioma1(e.target.value)}
+        />
+      </div>
+<br />
+<div>
+        <input
+          type="text"
+          placeholder="Idioma 2"
+          value={idiomas2}
+          onChange={(e) => setIdioma2(e.target.value)}
+        />
+      </div>
+<br />
+<div>
+        <input
+          type="text"
+          placeholder="Informatica"
+          value={informatica}
+          onChange={(e) => setInfo(e.target.value)}
+        />
+      </div>
+<br />
       <div>
         <label>Documento:</label>
         <input
