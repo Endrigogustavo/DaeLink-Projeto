498b85edb2465831ed3a3e6097a5d1d4b88d384c
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-11 15:24:34 -0300

    Criação das funcionalidades

--- Src/Apresentation/Web/src/Pages/Empresa/Vagas/AdicionarPessoa.jsx
+++ Src/Apresentation/Web/src/Pages/Empresa/Vagas/AdicionarPessoa.jsx
@@ -1,53 +1,175 @@
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import { auth, db } from '../../../Database/Firebase';
 import { onAuthStateChanged } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
-
+import { doc, getDocs, collection, addDoc, query, where } from 'firebase/firestore';
 const UserData = () => {
+  const navigate = useNavigate()
+  const { id } = useParams();
+  const [userInfo, setUserInfo] = useState(null);
   const [user, setUser] = useState(null);
-  const [userData, setUserData] = useState(null);
+  const [vagas, setVagas] = useState([]);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
       if (currentUser) {
+        setUser(currentUser);
         fetchUserData(currentUser.uid);
+      } else {
+        setUser(null);
       }
     });
 
     return () => unsubscribe();
   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const vagasRef = collection(db, 'PCD');
+        const querySnapshot = await getDocs(vagasRef);
 
-  const fetchUserData = async (userId) => {
-    try {
-      const userDoc = await getDoc(doc(db, 'Empresas', userId));
-      if (userDoc.exists()) {
-        setUserData(userDoc.data());
-      } else {
-        console.log('No such document!');
+        const vagasListUser = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        setUserInfo(vagasListUser);
+      } catch (error) {
+        console.error('Error getting document:', error);
       }
+    };
+
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    const fetchVagas = async () => {
+      if (user) {
+        try {
+          const vagasRef = collection(db, 'Vagas');
+          const q = query(vagasRef, where('empresaId', '==', user.uid));
+          const querySnapshot = await getDocs(q);
+
+          const vagasList = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+          }));
+
+          setVagas(vagasList);
+        } catch (error) {
+          console.error('Error fetching vagas:', error);
+        }
+      }
+    };
+
+    fetchVagas();
+  }, [user]);
+
+  const AddUserToVaga = async (VagaId, IdEmpresa) => {
+    try {
+      const userToAdd = userInfo[0];
+
+      const vagaRef = doc(db, "Vagas", VagaId);
+      const candidatosRef = collection(vagaRef, 'candidatos');
+      await addDoc(candidatosRef, {
+        userId: userToAdd.id,
+        nome: userToAdd.name,
+        email: userToAdd.email
+      });
+
+      alert("Pessoa adicionada com sucesso!");
+      navigate(`/homeempresa/${IdEmpresa}`)
     } catch (error) {
-      console.error('Error getting document:', error);
+      console.error("Erro ao adicionar pessoa:", error);
     }
   };
 
+
   return (
-    <div>
-      {user ? (
-        <div>
-          <h1>Welcome, {user.email}</h1>
-          {userData && (
-            <div>
-              <h2>User Data:</h2>
-              <pre>{JSON.stringify(userData, null, 2)}</pre>
-            </div>
-          )}
+    <div className="bg-white p-8 rounded-md w-full">
+      <div>
+        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+          <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+            <table className="min-w-full leading-normal">
+              <thead>
+                <tr>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Vaga
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Area
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Empresa
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Exigencias
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Salario
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Tipo
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Local
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {vagas.map((vaga) => (
+                  <tr>
+
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <div className="flex items-center">
+                        <div className="ml-3">
+                          <p className="text-gray-900 whitespace-no-wrap">
+                            {vaga.id}
+                          </p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {vaga.area}
+                      </p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {vaga.empresa}
+                      </p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {vaga.exigencias}
+                      </p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {vaga.salario}
+                      </p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {vaga.tipo}
+                      </p>
+                    </td>
+                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <p className="text-gray-900 whitespace-no-wrap">
+                        {vaga.local}
+                      </p>
+                    </td>
+                    <button onClick={() => AddUserToVaga(vaga.id, vaga.empresaId)} type="submit" className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      <svg aria-hidden="true" className="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Candidatos
+                    </button>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      ) : (
-        <h1>Please log in</h1>
-      )}
+      </div>
     </div>
   );
 };
--- Src/Apresentation/Web/src/Pages/Empresa/Vagas/VisualizarDocumentos.jsx
+++ Src/Apresentation/Web/src/Pages/Empresa/Vagas/VisualizarDocumentos.jsx
@@ -4,8 +4,8 @@ import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { db } from '../../../Database/Firebase';
 
 
-function VisualizarPessoas() {
-    const { vagaId, id } = useParams();
+function VisualizarDocumentos() {
+  const { vagaId, id } = useParams();
   const [candidatos, setCandidatos] = useState([]);
   const [vaga, setVaga] = useState(null);
   const [error, setError] = useState('');
@@ -14,14 +14,14 @@ function VisualizarPessoas() {
     const fetchCandidatos = async () => {
       try {
         if (vagaId) {
-          const candidatosCollection = collection(db, 'Vagas', vagaId, 'candidatos', id ,'documentos');
-          console.log('candidatosCollection:', candidatosCollection); 
+          const candidatosCollection = collection(db, 'Vagas', vagaId, 'candidatos', id, 'documentos');
+          console.log('candidatosCollection:', candidatosCollection);
           const candidatosSnapshot = await getDocs(candidatosCollection);
           const candidatosList = candidatosSnapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data()
           }));
-          console.log('Candidatos:', candidatosList); 
+          console.log('Candidatos:', candidatosList);
           setCandidatos(candidatosList);
         } else {
           setError('ID da vaga não fornecido');
@@ -36,11 +36,11 @@ function VisualizarPessoas() {
       try {
         if (vagaId) {
           const vagaDoc = doc(db, 'Vagas', vagaId);
-          console.log('vagaDoc:', vagaDoc); 
+          console.log('vagaDoc:', vagaDoc);
           const vagaSnapshot = await getDoc(vagaDoc);
           if (vagaSnapshot.exists()) {
             const vagaData = { id: vagaSnapshot.id, ...vagaSnapshot.data() };
-            console.log('Vaga:', vagaData); 
+            console.log('Vaga:', vagaData);
             setVaga(vagaData);
           } else {
             console.log('Nenhum documento encontrado!');
@@ -59,64 +59,66 @@ function VisualizarPessoas() {
     fetchVaga();
   }, [vagaId]);
 
-  {candidatos.map(candidato => (
-    <li key={candidato.id}>{candidato.nome}</li>
-  ))}
+  {
+    candidatos.map(candidato => (
+      <li key={candidato.id}>{candidato.nome}</li>
+    ))
+  }
 
-    return (
-   <>
-        <div className="bg-white p-8 rounded-md w-full">
-                <div>
-                    <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-                        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                            <table className="min-w-full leading-normal">
-                                <thead>
-                                    <tr>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                            Nome
-                                        </th>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                            Email
-                                        </th>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                            Documentos
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                {candidatos.map(candidato => (
-                                        <tr>
+  return (
+    <>
+      <div className="bg-white p-8 rounded-md w-full">
+        <div>
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <thead>
+                  <tr>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Nome
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Email
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Documentos
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {candidatos.map(candidato => (
+                    <tr>
 
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <div className="flex items-center">
-                                                    <div className="ml-3">
-                                                        <p className="text-gray-900 whitespace-no-wrap">
-                                                            {candidato.nome}
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p className="text-gray-900 whitespace-no-wrap">
-                                                    {candidato.email}
-                                                </p>
-                                            </td>
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                            <button onClick={() => handleButtonClick(candidato.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-												<svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Documentos
-											</button>
-                                            </td>
-                                        
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div className="flex items-center">
+                          <div className="ml-3">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {candidato.nome}
+                            </p>
+                          </div>
                         </div>
-                    </div>
-                </div>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {candidato.email}
+                        </p>
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <button onClick={() => handleButtonClick(candidato.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Documentos
+                        </button>
+                      </td>
+
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-   </>
-      );
-    };
-    
-export default VisualizarPessoas;
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VisualizarDocumentos;
