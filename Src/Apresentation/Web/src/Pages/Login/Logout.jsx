505c440a4a81f1ddfce10a257b9b79ff6ec86102
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-15 11:57:16 -0300

    Atualização dos comentarios

--- Src/Apresentation/Web/src/Pages/Login/Logout.jsx
+++ Src/Apresentation/Web/src/Pages/Login/Logout.jsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { logout } from '../../Auth/Auth';
 
 const Logout = () => {
+  //Função de navegação do site
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    //Função do Auth.jsx para deslogar
     logout();
     // Redireciona para a página de login após o logout
-    navigate('/'); 
+    navigate('/');
   };
 
   return (
