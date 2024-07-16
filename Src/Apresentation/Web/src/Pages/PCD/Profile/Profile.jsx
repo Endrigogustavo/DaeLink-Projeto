7c80cd52ac3155213510222796c07b384e26372f
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-07-16 12:05:20 -0300

    Criação das telas de editar

--- Src/Apresentation/Web/src/Pages/PCD/Profile/Profile.jsx
+++ Src/Apresentation/Web/src/Pages/PCD/Profile/Profile.jsx
@@ -1,10 +1,13 @@
 import './Profile.css'
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { db } from '../../../Database/Firebase';
 import { doc, getDoc } from 'firebase/firestore';
+import { logout } from '../../../Auth/Auth';
+
 
 function Profile() {
+    const navigate = useNavigate();
     //Utilizado para pegar o id do usuario e da vaga na tela anterior
     const { id } = useParams();
     //Variaveis onde as informações serão setadas
@@ -33,11 +36,26 @@ function Profile() {
         return <div>Loading...</div>;
     }
 
+    function LogoutProfile() {
+        var response = confirm("Deseja fazer Logout?");
+        if (response == true) {
+            //Função do Auth.jsx para deslogar
+            logout();
+            // Redireciona para a página de login após o logout
+            navigate('/');
+        }
+
+    }
+    const EditProfile = (id) =>{
+        navigate(`/edituser/${id}`)
+    }
+
     return (
         <>
-            <div class="w-full background h-[500px]">
-                <img src="https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg" class="w-full h-full rounded-tl-lg rounded-tr-lg" />
+            <div className="w-full background h-[700px]">
+                <img src={userProfile.imageProfile} className="w-full h-full rounded-tl-lg rounded-tr-lg object-cover" />
             </div>
+
             <div class="container mx-auto my-5 p-5">
                 <div class="md:flex profile no-wrap md:-mx-2 ">
                     <div class="w-full md:w-4/12 md:mx-2">
@@ -49,8 +67,8 @@ function Profile() {
                             </div>
                             <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">{userProfile.name}</h1>
                             <h3 class="text-gray-600 font-lg text-semibold leading-6">{userProfile.trabalho}</h3>
-                            <p class="text-sm text-gray-500 hover:text-gray-600 leading-6">Lorem ipsum dolor sit amet
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <p class="text-sm text-gray-500 hover:text-gray-600 leading-6">
+                                {userProfile.sobre}
                             </p>
 
                         </div>
@@ -66,33 +84,7 @@ function Profile() {
                                             d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                     </svg>
                                 </span>
-                                <span>Similar Profiles</span>
-                            </div>
-                            <div class="grid grid-cols-3">
-                                <div class="text-center my-2">
-                                    <img class="h-16 w-16 rounded-full mx-auto"
-                                        src="https://cdn.australianageingagenda.com.au/wp-content/uploads/2015/06/28085920/Phil-Beckett-2-e1435107243361.jpg"
-                                        alt="" />
-                                    <a href="#" class="text-main-color">Kojstantin</a>
-                                </div>
-                                <div class="text-center my-2">
-                                    <img class="h-16 w-16 rounded-full mx-auto"
-                                        src="https://cdn.australianageingagenda.com.au/wp-content/uploads/2015/06/28085920/Phil-Beckett-2-e1435107243361.jpg"
-                                        alt="" />
-                                    <a href="#" class="text-main-color">James</a>
-                                </div>
-                                <div class="text-center my-2">
-                                    <img class="h-16 w-16 rounded-full mx-auto"
-                                        src="https://cdn.australianageingagenda.com.au/wp-content/uploads/2015/06/28085920/Phil-Beckett-2-e1435107243361.jpg"
-                                        alt="" />
-                                    <a href="#" class="text-main-color">Natie</a>
-                                </div>
-                                <div class="text-center my-2">
-                                    <img class="h-16 w-16 rounded-full mx-auto"
-                                        src="https://bucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com/public/images/f04b52da-12f2-449f-b90c-5e4d5e2b1469_361x361.png"
-                                        alt="" />
-                                    <a href="#" class="text-main-color">Casey</a>
-                                </div>
+
                             </div>
                         </div>
 
@@ -102,9 +94,18 @@ function Profile() {
                         <div class="sm:block hidden">
                             <button
                                 type="button"
+                                onClick={() => EditProfile(id)}
                                 class="flex button-profile -mt-12 w-auto cursor-pointer select-none appearance-none items-center justify-center space-x-1 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 transition hover:border-gray-300 focus:border-gray-300 focus:outline-none focus:ring-0"
                             >
-                                Edit profile
+                                Editar Perfil
+                            </button>
+                            <br /><br />
+                            <button
+                                type="button"
+                                onClick={LogoutProfile}
+                                class="flex button-profile -mt-12 w-auto cursor-pointer select-none appearance-none items-center justify-center space-x-1 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 transition hover:border-gray-300 focus:border-gray-300 focus:outline-none focus:ring-0"
+                            >
+                                Logout
                             </button>
                             <br />
                             <div class="bg-white p-3 shadow-sm rounded-sm">
@@ -149,8 +150,9 @@ function Profile() {
                                             <span class="tracking-wide">Experiencias</span>
                                         </div>
                                         <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                        </p>
+                                            {
+                                                userProfile.experiencias
+                                            } </p>
                                     </div>
                                     <div>
                                         <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
@@ -187,8 +189,8 @@ function Profile() {
                                                     </li>
 
                                                     <li class="flex border-b py-2">
-                                                        <span class="font-bold w-24">Mobile:</span>
-                                                        <span class="text-gray-700">(123) 123-1234</span>
+                                                        <span class="font-bold w-24">Deficiencia:</span>
+                                                        <span class="text-gray-700">{userProfile.deficiencia}</span>
                                                     </li>
                                                     <li class="flex border-b py-2">
                                                         <span class="font-bold w-24">Email:</span>
