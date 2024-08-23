21228bfca03098e49112521077cb3c11ad57b65c
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-08-23 09:27:58 -0300

    Teste de ssl de produção

--- Src/Apresentation/Web/vite.config.js
+++ Src/Apresentation/Web/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    }
+  }
+});
