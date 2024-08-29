fb77dd88ce7057cad3eae7ae7570fd81008b894a
SonExpedito <alexexpeditoff7@gmail.com>
2024-08-28 21:15:57 -0300

    404 Finalizada

--- Src/Apresentation/Web/src/Pages/Error/TelaErro404.jsx
+++ Src/Apresentation/Web/src/Pages/Error/TelaErro404.jsx
@@ -9,14 +9,15 @@ const Tela404 = () => {
     return (
         <>
             <div className="w-screen h-screen bg-gray-900 flex">
-                <div className="w-5/12 h-full flex items-center justify-center bg-gray-300">
+                <div className="w-5/12 h-full flex items-center justify-center bg-gray-300 img-container">
                     <img src="https://i.postimg.cc/0NdyNLyF/Bellingham.png" className="" alt="" />
                 </div>
 
-                <div className="w-7/12 h-full flex items-center flex-col justify-center gap-2">
+                <div className="w-7/12 h-full flex items-center  side-container flex-col justify-center gap-2">
+                    <img src="https://i.postimg.cc/nh7bpn6C/pnglogo.png" alt=""  className="hidden logoimg h-28 pb-4"/>
                     <h1 className="text-4xl text-white font-medium">Erro 404</h1>
                     <p className="text-white text-justify text-lg opacity-80">Nada Encontrado</p>
-                    <p className="text-white text-justify">Talvez essa página não exista ou tenha sido movida</p>
+                    <p className="text-white text-justify r">Talvez essa página não exista ou tenha sido movida</p>
                     <button
                         type="submit"
                         className='w-44 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all'
