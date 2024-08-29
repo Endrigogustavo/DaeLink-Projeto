8d9f9e14617bf94876a8736a4ff7e4f46c46bf43
SonExpedito <alexexpeditoff7@gmail.com>
2024-08-28 21:02:47 -0300

    TelaErro404

--- Src/Apresentation/Web/src/Pages/Error/TelaErro404.jsx
+++ Src/Apresentation/Web/src/Pages/Error/TelaErro404.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './style.css'
-import Img from './fundoImg.jpg'
+
 
 //https://www.deviantart.com/wabisabiwonders/gallery
 
@@ -8,22 +8,23 @@ const Tela404 = () => {
 
     return (
         <>
-            <div className="gradient text-white min-h-screen flex items-center">
-                <div className="container mx-auto p-4 flex flex-wrap items-center">
-                    <div className="w-full md:w-5/12 text-center p-4">
-                        <img src={Img} alt="Not Found" />
-                    </div>
-                    <div className="w-full md:w-7/12 text-center md:text-left p-4">
-                        <div className="text-6xl font-medium">404</div>
-                        <div className="text-xl md:text-3xl font-medium mb-4">
-                            Oops. This page has gone missing.
-                        </div>
-                        <div className="text-lg mb-8">
-                            You may have mistyped the address or the page may have moved.
-                        </div>
-                        <a href="#" className="border border-white rounded p-4">Go Home</a>
-                    </div>
+            <div className="w-screen h-screen bg-gray-900 flex">
+                <div className="w-5/12 h-full flex items-center justify-center bg-gray-300">
+                    <img src="https://i.postimg.cc/0NdyNLyF/Bellingham.png" className="" alt="" />
                 </div>
+
+                <div className="w-7/12 h-full flex items-center flex-col justify-center gap-2">
+                    <h1 className="text-4xl text-white font-medium">Erro 404</h1>
+                    <p className="text-white text-justify text-lg opacity-80">Nada Encontrado</p>
+                    <p className="text-white text-justify">Talvez essa página não exista ou tenha sido movida</p>
+                    <button
+                        type="submit"
+                        className='w-44 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all'
+                    >
+                        Voltar
+                    </button>
+                </div>
+
             </div>
         </>
     )
