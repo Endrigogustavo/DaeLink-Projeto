6459899ed5655f6ca3afe28e69367af5a402f262
SonExpedito <alexexpeditoff7@gmail.com>
2024-09-08 13:46:13 -0300

    DocumentosPage 80%

--- Src/Apresentation/Web/src/Pages/PCD/Vagas/EnviarDocumentos/DocumentosForm.jsx
+++ Src/Apresentation/Web/src/Pages/PCD/Vagas/EnviarDocumentos/DocumentosForm.jsx
@@ -1,12 +1,14 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { doc, collection, addDoc, getDocs, query, where } from "firebase/firestore";
 import { db, storage } from "../../../../Database/Firebase";
 import { useNavigate } from 'react-router-dom';
 import { uploadBytes, ref, getDownloadURL } from 'firebase/storage';
 import DocumentosStates from "./DocumentosStates";
-import { FaFile  } from "react-icons/fa6";
+import { FaFile } from "react-icons/fa6";
+import { IoAddCircleSharp } from "react-icons/io5";
 
 const DocumentosForm = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
     const {
         userId, setUserId,
         vagaUid, setVagaUid,
@@ -22,14 +24,9 @@ const DocumentosForm = () => {
         formacao1a, setFormacao1a,
         formacao2a, setFormacao2a,
         formacao3a, setFormacao3a,
-        formacao1c, setFormacao1c,
-        formacao2c, setFormacao2c,
-        formacao3c, setFormacao3c,
         qualificacao1, setQualificacao1,
         qualificacao2, setQualificacao2,
         qualificacao3, setQualificacao3,
-        qualificacao4, setQualificacao4,
-        qualificacao5, setQualificacao5,
         idiomas1, setIdiomas1,
         idiomas2, setIdiomas2,
         informatica, setInformatica,
@@ -38,6 +35,7 @@ const DocumentosForm = () => {
 
     const navigate = useNavigate();
     const textareaRef = useRef(null);
+    const inputFileRef = useRef(null);
 
     const adjustTextareaHeight = (ref) => {
         if (ref.current) {
@@ -52,16 +50,24 @@ const DocumentosForm = () => {
         }
     }, []);
 
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setDocumento(file);  // Atualize o estado do documento
+        }
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            if (!documento) {
+            if (!selectedFile) {
                 alert("Por favor, selecione um documento para enviar.");
                 return;
             }
 
-            const storageRef = ref(storage, `documentos/${documento.name}`);
-            await uploadBytes(storageRef, documento);
+            const storageRef = ref(storage, `documentos/${selectedFile.name}`);
+            await uploadBytes(storageRef, selectedFile);
             const downloadURL = await getDownloadURL(storageRef);
 
             const candidatosRef = collection(db, "Vagas", vagaUid, "candidatos");
@@ -87,9 +93,6 @@ const DocumentosForm = () => {
                     formacao_academica1: formacao1a,
                     formacao_academica2: formacao2a,
                     formacao_academica3: formacao3a,
-                    formacao_complementar1: formacao1c,
-                    formacao_complementar2: formacao2c,
-                    formacao_complementar3: formacao3c,
                     qualificacao1,
                     qualificacao2,
                     qualificacao3,
@@ -103,6 +106,7 @@ const DocumentosForm = () => {
                 });
 
                 alert("Documento adicionado com sucesso!");
+                setSelectedFile(null);
                 setDocumento(null);
                 navigate(`/homeuser/${userId}`);
             } else {
@@ -120,20 +124,19 @@ const DocumentosForm = () => {
             <div className="h-64 w-full flex items-center justify-center">
                 <div className='w-3/12 h-32 shadow-2xl bg-white border-gray-700 border-4 rounded-full flex overflow-hidden px-4'>
                     <div className='w-2/6 h-full flex items-center justify-center'>
-                        <FaFile  className='text-5xl text-gray-900  text-center' />
+                        <FaFile className='text-5xl text-gray-900 text-center' />
                     </div>
                     <div className='w-5/6 h-full flex items-center justify-center flex-col'>
                         <p className='font-medium text-lg text-center'>Envio de Documentos</p>
-                        <p className='font-normal text-base text-left w-4/4'>Coloque aqueles que comprovem sua 
-                            Deficiência, Currículo, Dipliomas...
+                        <p className='font-normal text-base text-left w-full'>Coloque aqueles que comprovem sua
+                            Deficiência, Currículo, Diplomas...
                         </p>
                     </div>
                 </div>
-
             </div>
             <form onSubmit={handleSubmit} className="h-fit w-full grid grid-cols-2 gap-y-2 items-center justify-items-center py-8">
 
-                <div className="flex flex-col ">
+                <div className="flex flex-col">
                     <label className="text-lg font-medium">Nome</label>
                     <input
                         type="text"
@@ -189,14 +192,41 @@ const DocumentosForm = () => {
                     />
                 </div>
 
+                <div className="flex flex-col">
+                    <label className="text-lg font-medium items-center flex gap-2">
+                        Formações
+                        <p className="opacity-80 text-sm">limite 3</p>
+                    </label>
+                    <div className="flex gap-2">
+                        <div>
+                            <label
+                                htmlFor="doc-input"
+                                className='w-16 h-fit py-2 border-2 border-blue-500 font-bold rounded-xl transition-all hover:bg-blue-500 cursor-pointer hover:text-white flex items-center justify-center'
+                            >
+                                <IoAddCircleSharp size={32} />
+                            </label>
+                            <input
+                                id="doc-input"
+                                type="file"
+                                className='hidden'
+                                accept=".pdf,.doc,.docx"
+                                ref={inputFileRef}
+                                onChange={handleFileChange}
+                                multiple
+                            />
+                        </div>
 
-                <div className="flex flex-col mt-4">
-                    <label className="text-lg font-medium">Documento:</label>
-                    <input
-                        type="file"
-                        onChange={(e) => setDocumento(e.target.files[0])}
-                    />
+                        {selectedFile && (
+                            <div className="flex">
+                                <label className='w-fit h-fit py-5 px-3 border-2 border-blue-500 font-bold rounded-xl flex flex-col items-center justify-center'>
+                                    <FaFile size={32} />
+                                    <p>{selectedFile.name}</p>
+                                </label>
+                            </div>
+                        )}
+                    </div>
                 </div>
+
                 <button
                     type="submit"
                     className="w-52 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all"
@@ -205,7 +235,6 @@ const DocumentosForm = () => {
                 </button>
             </form>
         </>
-
     );
 };
 
