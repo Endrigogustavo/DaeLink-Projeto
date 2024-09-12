e0b2c740415bb6881e3c150860630945be594f16
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-09-12 18:54:30 -0300

    Criação do manter PCD

--- Src/Apresentation/Web/src/Pages/Empresa/Vagas/Visualizar_Processos.jsx
+++ Src/Apresentation/Web/src/Pages/Empresa/Vagas/Visualizar_Processos.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, deleteDoc } from 'firebase/firestore';
 import { db } from '../../../Database/Firebase';
 import CarregamentoTela from "../../../Components/TelaCarregamento/Carregamento"
 
@@ -55,6 +55,18 @@ function Visualizar_Processo() {
     const handleButtonClick = (vagaId) => {
         navigate(`/visualizarpessoas/${vagaId}`);
     };
+
+    const DeleteVaga = async(vagaId) =>{
+        const response =  confirm("Deseja deletar a vaga?")
+        if(response == true){
+            try {
+                const GetVagaInfo = doc(db, "Vagas", vagaId)
+                await deleteDoc(GetVagaInfo)
+            } catch (error) {
+                alert("Erro: ", error)
+            }
+        }
+    } 
 
     return (
         <>
@@ -133,6 +145,14 @@ function Visualizar_Processo() {
                                             </td>
                                             <button onClick={() => handleButtonClick(vaga.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Candidatos
+                                            </button>
+
+                                            <button onClick={() => handleButtonClick(vaga.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-purple-700 border border-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                                                <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Atulizar
+                                            </button>
+
+                                            <button onClick={() => DeleteVaga(vaga.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-red-700 border border-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                                <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Deletar
                                             </button>
                                         </tr>
                                     ))}
