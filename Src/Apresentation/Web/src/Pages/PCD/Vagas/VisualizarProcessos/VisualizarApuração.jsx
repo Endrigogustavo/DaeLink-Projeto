d88252cf57978156444dc00e25bb33eaeb4c5770
SonExpedito <alexexpeditoff7@gmail.com>
2024-09-11 22:10:46 -0300

    VisualizarApuração - 50%

--- Src/Apresentation/Web/src/Pages/PCD/Vagas/VisualizarProcessos/VisualizarApuração.jsx
+++ Src/Apresentation/Web/src/Pages/PCD/Vagas/VisualizarProcessos/VisualizarApuração.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../../../../Database/Firebase';
 import CarregamentoTela from "../../../../Components/TelaCarregamento/Carregamento"
 import { decrypt } from '../../../../Security/Cryptography_Rotes';
+import Navbar from '../../Navbar/Navbar';
+
+import {
+    FaSquareGithub, FaSquareXTwitter, FaSquareInstagram
+} from "react-icons/fa6";
+
+
 
 function Visualizar_Processo() {
     //Pegar o id do usuario na tela anterior
@@ -47,7 +54,7 @@ function Visualizar_Processo() {
     }, [id]);
 
     if (loading) {
-        return <CarregamentoTela/>;
+        return <CarregamentoTela />;
     }
 
     if (vagas.length === 0) {
@@ -60,41 +67,30 @@ function Visualizar_Processo() {
 
     return (
         <>
-            <div className="bg-white p-8 rounded-md w-full">
-                <div>
-                    <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-                        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                            <table className="min-w-full leading-normal">
-                                <thead>
-                                    <tr>
-                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                            Apuração
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {vagas.map((vaga) => (
-                                        <tr>
+            <Navbar />
 
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <div className="flex items-center">
-                                                    <div className="ml-3">
-                                                        <p className="text-gray-900 whitespace-no-wrap">
-                                                            {vaga.situação}
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+            <div className='w-full h-apuracaostatus flex items-center justify-center'>
+                <div className='w-fit h-fit bg-gray-800 rounded-xl p-8 flex flex-col'>
+                    {vagas.map((vaga) => (
+                        <div key={vaga.id}> {/* Adiciona a key aqui */}
+                            <h1 className={`w-40 text-white text-center font-bold text-sm py-2 px-4 rounded-full ${vaga.situação ? 'bg-blue-700' : 'bg-red-600'}`}>
+                                {vaga.situação ? vaga.situação : 'Pendente'}
+                            </h1>
                         </div>
-                    </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="w-full bg-gray-900 h-16 ">
+                <div className="flex w-full h-full items-center justify-center gap-4">
+                    <Link><FaSquareInstagram className="text-3xl text-gray-200 opacity-80  mediahover" /></Link>
+                    <Link><FaSquareXTwitter className="text-3xl text-gray-200 opacity-80 mediahover" /></Link>
+                    <Link><FaSquareGithub className="text-3xl text-gray-200 opacity-80 mediahover" /></Link>
                 </div>
             </div>
         </>
     );
+
 }
 
 export default Visualizar_Processo;
