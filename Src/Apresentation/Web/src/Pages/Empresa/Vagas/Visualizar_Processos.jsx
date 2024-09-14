479da82f2e064dfc569577ac5bb8689feb672442
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-09-14 16:33:37 -0300

    Criação dos manter

---
+++ Src/Apresentation/Web/src/Pages/Empresa/Vagas/Visualizar_Processos.jsx
@@ -0,0 +1,169 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { collection, query, where, getDocs, deleteDoc } from 'firebase/firestore';
+import { db } from '../../../Database/Firebase';
+import CarregamentoTela from "../../../Components/TelaCarregamento/Carregamento"
+
+function Visualizar_Processo() {
+    //Pegar o id do usuario na tela anterior
+    const { id } = useParams();
+    //Variaveis para setar dados do banco
+    const [vagas, setVagas] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    //Função de navegação do site
+    const navigate = useNavigate()
+
+    //useEffect é utilizado por ser chamado toda vez que o site for renderizado (F5)
+    useEffect(() => {
+        const GetVagas = async () => {
+            try {
+                //Caminho dos dados da tabela PCD do banco
+                const VagasCollection = collection(db, 'Vagas');
+                //Tratando a pesquisa com query e where para pegar os dados de uma empresa 
+                const QueryVagas = query(VagasCollection, where('empresaId', '==', id));
+                //Pegando dados
+                const GetVagasResult = await getDocs(QueryVagas);
+
+                //Utilizando a funçã0 map para guardar os valos para serem listados
+                const vagasList = GetVagasResult.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+
+                //Setando resultado em uma variavel
+                setVagas(vagasList);
+                console.log(vagasList)
+            } catch (error) {
+                console.error('Erro ao buscar vagas: ', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        GetVagas();
+    }, [id]);
+
+    if (loading) {
+        return <CarregamentoTela/>;
+    }
+
+    if (vagas.length === 0) {
+        return <div>Nenhuma vaga encontrada para esta empresa.</div>;
+    }
+
+    const handleButtonClick = (vagaId) => {
+        navigate(`/visualizarpessoas/${vagaId}`);
+    };
+
+    const DeleteVaga = async(vagaId) =>{
+        const response =  confirm("Deseja deletar a vaga?")
+        if(response == true){
+            try {
+                const GetVagaInfo = doc(db, "Vagas", vagaId)
+                await deleteDoc(GetVagaInfo)
+            } catch (error) {
+                alert("Erro: ", error)
+            }
+        }
+    } 
+
+    return (
+        <>
+            <div className="bg-white p-8 rounded-md w-full">
+                <div>
+                    <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                        <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                            <table className="min-w-full leading-normal">
+                                <thead>
+                                    <tr>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Vaga
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Area
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Empresa
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Exigencias
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Salario
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Tipo
+                                        </th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Local
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {vagas.map((vaga) => (
+                                        <tr>
+
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <div className="flex items-center">
+                                                    <div className="ml-3">
+                                                        <p className="text-gray-900 whitespace-no-wrap">
+                                                            {vaga.id}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    {vaga.area}
+                                                </p>
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    {vaga.empresa}
+                                                </p>
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    {vaga.exigencias}
+                                                </p>
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    {vaga.salario}
+                                                </p>
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    {vaga.tipo}
+                                                </p>
+                                            </td>
+                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    {vaga.local}
+                                                </p>
+                                            </td>
+                                            <button onClick={() => handleButtonClick(vaga.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Candidatos
+                                            </button>
+
+                                            <button onClick={() => handleButtonClick(vaga.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-purple-700 border border-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+                                                <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Atulizar
+                                            </button>
+
+                                            <button onClick={() => DeleteVaga(vaga.id)} type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-red-700 border border-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                                <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Deletar
+                                            </button>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Visualizar_Processo;
