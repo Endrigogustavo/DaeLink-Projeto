7baf272e6a830c682c39e3e60e764afbbee70c56
SonExpedito <alexexpeditoff7@gmail.com>
2024-09-20 14:39:54 -0300

    Chat50%

--- Src/Apresentation/Web/src/Pages/PCD/Chat/Chat.jsx
+++ Src/Apresentation/Web/src/Pages/PCD/Chat/Chat.jsx
@@ -2,29 +2,17 @@
 import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth"
 import { useCollectionData } from "react-firebase-hooks/firestore"
 import "./ChatStyle.css"
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { auth, db } from "../../../Database/Firebase"
-import { addDoc, collection, orderBy, query, where, serverTimestamp , limit, getDocs} from "firebase/firestore";
+import { addDoc, collection, orderBy, query, where, serverTimestamp, limit, getDocs } from "firebase/firestore";
 import { useState, useRef, useEffect } from "react";
 import { decrypt } from "../../../Security/Cryptography_Rotes";
 
-const App = () => {
-    const [user] = useAuthState(auth)
-    return (
-        <div className="App">
-            <header className="header">
-                <h1>ReactChat</h1>
-                <SingOut />
-            </header>
-            <section className="sectionchat">{user ? <ChatRoom/> : <SignIn />}</section>
-        </div>
-    );
-};
-
-export default App;
+import { FaCircleArrowLeft } from "react-icons/fa6";
+import { IoSend } from "react-icons/io5";
 
 const ChatRoom = () => {
-    
+
     const { encryptedId, empresaId } = useParams();
     const decryptedId = decodeURIComponent(decrypt(encryptedId))
     const dummy = useRef();
@@ -37,10 +25,10 @@ const ChatRoom = () => {
         alert(decryptedId)
         const GetChatMessage = async () => {
             const ChatCollection = collection(db, "Chat");
-            const GetQueryPCDId = query(ChatCollection, where("userId", "==",  decryptedId));
+            const GetQueryPCDId = query(ChatCollection, where("userId", "==", decryptedId));
             const GetQueryCompanyId = query(GetQueryPCDId, where("empresaId", "==", empresaId));
             const GetChatWithCompanyAndPCD = await getDocs(GetQueryCompanyId);
-            
+
             if (!GetChatWithCompanyAndPCD.empty) {
                 const DocumentRef = GetChatWithCompanyAndPCD.docs[0]?.ref;
                 if (DocumentRef) {
@@ -86,35 +74,50 @@ const ChatRoom = () => {
         }
     }, [collectionData]);
 
+    const handleButtonClickReturn = () => {
+        navigate(-1); // Simplesmente volta para a página anterior
+    };
+
     return (
         <>
-            <main className="mainchat">
-                {messages &&
-                    messages.map((msg, index) => (
-                        <ChatMessage key={index} message={msg} />
-                    ))}
-                <div ref={dummy}></div>
-            </main>
-            <form className="formchat" onSubmit={CreateNewMessage}>
-                <input
-                    type="text"
-                    className="inputchat"
-                    value={formValue}
-                    onChange={(e) => setFormValue(e.target.value)}
-                />
-                <button className="buttonchat">Enviar</button>
-            </form>
+            <div className="h-screen w-full flex flex-col items-center justify-center overflow-hidden">
+                <div className="w-full h-24 flex items-center bg-gray-700 px-12 gap-3">
+                    <button onClick={handleButtonClickReturn}>
+                        <FaCircleArrowLeft className="text-3xl text-white" />
+                    </button>
+                    <img src="https://t.ctcdn.com.br/aFp_I8ScTJJch32H29ImNebDEYU=/i489949.jpeg" alt="" className="w-12 h-12 rounded-full border-2 border-blue-500 " />
+                    <h1 className="font-medium text-white">Apple</h1>
+                </div>
+
+                <div className="Chatcontainer ">
+                    {messages &&
+                        messages.map((msg, index) => (
+                            <ChatMessage key={index} message={msg} />
+                        ))}
+                    <div ref={dummy}></div>
+                </div>
+
+                <form className="MessageForm  py-3" onSubmit={CreateNewMessage}>
+                    <input
+                        type="text"
+                        className=" h-full border-2 bg-gray-700 respon-w-input rounded-full flex w-4/5 text-white p-4 mt-1"
+                        value={formValue}
+                        onChange={(e) => setFormValue(e.target.value)}
+                    />
+                    <button className="w-12 h-12 rounded-full bg-blue-500 flex items-center justify-center"><IoSend className="text-white text-3xl" /></button>
+                </form>
+            </div>
         </>
     );
 };
 
 export const ChatMessage = (props) => {
-    const {text, photoURL, uid} = props.message
+    const { text, photoURL, uid } = props.message
 
     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
     return (
         <div className={`message ${messageClass}`}>
-            <img className="imgchat" src={photoURL}/>
+            <img className="imgchat" src={photoURL} />
             <p className="paragrafo">{text}</p>
         </div>
     )
@@ -123,14 +126,11 @@ export const ChatMessage = (props) => {
 
 export const SignIn = () => {
     const [SignInWithGoogle] = useSignInWithGoogle(auth)
-    return(
+    return (
         <button className="sign-in" onClick={() => SignInWithGoogle()}>
             Logar com google
         </button>
     )
 }
 
-export const SingOut = () => {
-    return auth.currentUser && (<button className="sing-out" onClick={() => auth.signOut()}>Sair</button>)
-    
-}
+export default ChatRoom;
--- Src/Apresentation/Web/src/Pages/PCD/Vagas/VisualizarProcessos/ProcessosList.jsx
+++ Src/Apresentation/Web/src/Pages/PCD/Vagas/VisualizarProcessos/ProcessosList.jsx
@@ -72,21 +72,21 @@ const ProcessosList = () => {
         const encryptedId = encodeURIComponent(encrypt(decryptedId))
         const VagaInfo = collection(db, "Vagas", vagaId, "candidatos")
         const QueryDocs = query(VagaInfo, where("userId", "==", decryptedId));
-    
+
         const DocResult = await getDocs(QueryDocs)
 
-        if(!DocResult.empty){
-           alert("Usuario encontrado")
-           const DocRef = collection(db, "Vagas", vagaId, "candidatos", DocResult.docs[0].id, "documentos")
-           const GetDoc = await getDocs(DocRef)
-           if(!GetDoc.empty){
-            alert("Documentos ja existe")
-            navigate(`/atualizardocumento/${encryptedId}/${vagaId}`);
-           }else{
-            alert("Sem documentos")
-            navigate(`/enviardocumento/${encryptedId}/${vagaId}`);
-           }
-        }else{
+        if (!DocResult.empty) {
+            alert("Usuario encontrado")
+            const DocRef = collection(db, "Vagas", vagaId, "candidatos", DocResult.docs[0].id, "documentos")
+            const GetDoc = await getDocs(DocRef)
+            if (!GetDoc.empty) {
+                alert("Documentos ja existe")
+                navigate(`/atualizardocumento/${encryptedId}/${vagaId}`);
+            } else {
+                alert("Sem documentos")
+                navigate(`/enviardocumento/${encryptedId}/${vagaId}`);
+            }
+        } else {
             alert("Usuario não encontrado")
         }
     };
@@ -104,11 +104,11 @@ const ProcessosList = () => {
         navigate(`/chatpcd/${encryptedId}/${empresaId}`);
     };
 
-  
+
 
     return (
         <>
-            <div className={`w-full h-fit flex justify-center items-center flex-col ${loading ? '' : 'grid Processoscontainer gap-4 justify-items-center items-center pb-4'}`}>
+            <div className={`w-full h-fit flex justify-center items-center flex-col ${loading ? '' : (vagas.length > 0 ? 'grid Processoscontainer gap-4 justify-items-center items-center pb-4' : 'flex pb-8')}`}>
                 {loading ? (
                     <div className="flex justify-center items-center h-64">
                         <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
@@ -117,7 +117,7 @@ const ProcessosList = () => {
                     Array.isArray(vagas) && vagas.length > 0 ? (
                         vagas.map((vaga) => {
                             return (
-                                <div key={vaga.id} className='h-processocard w-processocard  shadow-xl bg-gray-800 rounded-2xl flex  border-2 overflow-hidden'>
+                                <div key={vaga.id} className='h-processocard w-processocard shadow-xl bg-gray-800 rounded-2xl flex border-2 overflow-hidden'>
                                     {/* Render empresa details */}
                                     {vaga.empresa && (
                                         <div className='flex flex-col h-full w-2/6 justify-center items-center relative'>
@@ -126,44 +126,39 @@ const ProcessosList = () => {
                                             <h1 className='font-medium text-white text-center text-lg'>{vaga.empresa.name}</h1>
                                         </div>
                                     )}
-                                    {
-                                        <>
-                                            <div className='flex flex-col bg-gray-900 rounded-2xl h-full w-4/6 justify-center items-center overflow-hidden gap-2'>
-                                                <div className="w-full flex flex-col justify-center gap-1 ">
-                                                    <h1 className='font-medium text-xl text-center text-white'>{vaga.vaga}</h1>
-                                                    <button 
-                                                    onClick={() => ApuraçãoResultado(vaga.id)}>
-                                                    <h1 className={`w-32 text-white text-center font-bold text-sm py-2 px-2 rounded-full ${vaga.situação ? 'bg-blue-700' : 'bg-red-600'}`}>
-                                                        {vaga.situação ? vaga.situação : 'Pendente'}
-                                                    </h1>
-                                                    </button>
-                                                    <p className='text-white opacity-80 text-sm px-4 truncate'>Tipo: {vaga.tipo}  </p>
-                                                    <p className='text-white opacity-80 text-sm px-4 truncate'>Salário: {vaga.salario}</p>
-                                                </div>
-                                                <div className='w-full flex justify-center'>
-                                                    <button
-                                                        onClick={() => ChatEmpresa(vaga.empresaId)}
-                                                        type="submit"
-                                                        className='w-44 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all'
-                                                    >
-                                                        Chat
-                                                    </button>
-                                                    <button
-                                                        onClick={() => EnviarDoc(vaga.id)}
-                                                        type="submit"
-                                                        className='w-44 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all'
-                                                    >
-                                                        Enviar Documentos
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </>
-                                    }
+                                    <div className='flex flex-col bg-gray-900 rounded-2xl h-full w-4/6 justify-center items-center overflow-hidden gap-2'>
+                                        <div className="w-full flex flex-col justify-center gap-1">
+                                            <h1 className='font-medium text-xl text-center text-white'>{vaga.vaga}</h1>
+                                            <button onClick={() => ApuraçãoResultado(vaga.id)}>
+                                                <h1 className={`w-32 text-white text-center font-bold text-sm py-2 px-2 rounded-full ${vaga.situação ? 'bg-blue-700' : 'bg-red-600'}`}>
+                                                    {vaga.situação ? vaga.situação : 'Pendente'}
+                                                </h1>
+                                            </button>
+                                            <p className='text-white opacity-80 text-sm px-4 truncate'>Tipo: {vaga.tipo}</p>
+                                            <p className='text-white opacity-80 text-sm px-4 truncate'>Salário: {vaga.salario}</p>
+                                        </div>
+                                        <div className='w-full flex justify-center'>
+                                            <button
+                                                onClick={() => ChatEmpresa(vaga.empresaId)}
+                                                type="submit"
+                                                className='w-44 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all'
+                                            >
+                                                Chat
+                                            </button>
+                                            <button
+                                                onClick={() => EnviarDoc(vaga.id)}
+                                                type="submit"
+                                                className='w-44 bg-blue-700 hover:bg-blue-500 text-white font-bold text-sm py-2 px-4 rounded-full transition-all'
+                                            >
+                                                Enviar Documentos
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
                             );
                         })
                     ) : (
-                        <div className='w-3/12 h-32 shadow-2xl bg-white border-gray-700 border-4 rounded-full flex overflow-hidden px-4'>
+                        <div className='w-96 h-32 shadow-2xl bg-white border-gray-700 border-4 rounded-full flex overflow-hidden px-4'>
                             <div className='w-2/6 h-full flex items-center justify-center'>
                                 <FaSquareXmark className='text-5xl text-gray-900 text-center' />
                             </div>
@@ -176,7 +171,8 @@ const ProcessosList = () => {
                 )}
             </div>
         </>
-    )
+    );
+
 }
 
 export default ProcessosList;
