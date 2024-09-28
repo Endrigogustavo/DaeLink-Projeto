5e61c2f86fdf8b4512ee1262a6766c2d609717ec
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-09-28 18:56:35 -0300

    Atualizaçãp dashboard e watson

--- Src/Apresentation/Web/src/Watson/Watson.jsx
+++ Src/Apresentation/Web/src/Watson/Watson.jsx
@@ -3,19 +3,25 @@ import React, { useEffect } from 'react';
 const WatsonChat = () => {
   useEffect(() => {
     window.watsonAssistantChatOptions = {
-      integrationID: "a438860f-919d-4d3e-8d3b-70412c8c3481", // The ID of this integration.
-      region: "au-syd", // The region your integration is hosted in.
-      serviceInstanceID: "239fa452-0d3f-4d0e-aa93-273b15005239", // The ID of your service instance.
-      onLoad: async (instance) => { await instance.render(); }
+      integrationID: "a438860f-919d-4d3e-8d3b-70412c8c3481",
+      region: "au-syd",
+      serviceInstanceID: "239fa452-0d3f-4d0e-aa93-273b15005239",
+      onLoad: async (instance) => { await instance.render(); },
+      namespace: "unique-namespace-name" // Adicione um namespace único
     };
 
-    const script = document.createElement('script');
-    script.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-    document.head.appendChild(script);
+    if (!document.querySelector(`script[src*="WatsonAssistantChatEntry.js"]`)) {
+      const script = document.createElement('script');
+      script.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+      document.head.appendChild(script);
+    }
 
     return () => {
-      // Limpeza do script quando o componente é desmontado
-      document.head.removeChild(script);
+      console.log('Removing Watson Assistant script');
+      const existingScript = document.querySelector(`script[src*="WatsonAssistantChatEntry.js"]`);
+      if (existingScript) {
+        document.head.removeChild(existingScript);
+      }
     };
   }, []);
 
