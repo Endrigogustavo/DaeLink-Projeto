477e9956ffd37a36a8db2ab13571693e18b4e379
SonExpedito <alexexpeditoff7@gmail.com>
2024-10-02 11:23:45 -0300

    Correção-Home

--- Src/Apresentation/Web/src/Pages/Empresa/Home/AboutEmpresa.jsx
+++ Src/Apresentation/Web/src/Pages/Empresa/Home/AboutEmpresa.jsx
@@ -7,96 +7,96 @@ import { db } from '../../../Database/Firebase';
 import { doc, getDoc } from 'firebase/firestore';
 
 const AboutEmpresa = () => {
-    const [loading, setLoading] = useState(true); // Estado de carregamento
-    const [userProfile, setUserProfile] = useState(null);
-    const [userId, setUserId] = useState("");
-    const navigate = useNavigate();
-    const { id } = useParams();
+  const [loading, setLoading] = useState(true); // Estado de carregamento
+  const [userProfile, setUserProfile] = useState(null);
+  const [userId, setUserId] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    useEffect(() => {
-        const getUserProfile = async () => {
-            const userDoc = doc(db, "Empresa", id);
-            const userSnap = await getDoc(userDoc);
+  useEffect(() => {
+    const getUserProfile = async () => {
+      const userDoc = doc(db, "Empresa", id);
+      const userSnap = await getDoc(userDoc);
 
-            if (userSnap.exists()) {
-                setUserProfile(userSnap.data());
-                setUserId(userSnap.id);
-            } else {
-                setUserProfile(null);
-                alert("Sem documentos!");
-            }
-            setLoading(false); // Carregamento concluído
-        };
+      if (userSnap.exists()) {
+        setUserProfile(userSnap.data());
+        setUserId(userSnap.id);
+      } else {
+        setUserProfile(null);
+        alert("Sem documentos!");
+      }
+      setLoading(false); // Carregamento concluído
+    };
 
-        getUserProfile();
-    }, [id]);
-
-
-    if (loading) {
-        return <div>Loading...</div>; // Exibe um loading enquanto os dados estão sendo carregados
-    }
-
-    if (!userProfile) {
-        return <div>Perfil não encontrado</div>; // Exibe uma mensagem caso o perfil não seja encontrado
-    }
+    getUserProfile();
+  }, [id]);
 
 
-    const texto1 = `Analisar os processos de contatação ativos
+  if (loading) {
+    return <div>Loading...</div>; // Exibe um loading enquanto os dados estão sendo carregados
+  }
+
+  if (!userProfile) {
+    return <div>Perfil não encontrado</div>; // Exibe uma mensagem caso o perfil não seja encontrado
+  }
+
+
+  const texto1 = `Analisar os processos de contatação ativos
     irá trazer uma melhora constante para sua empresa, já que novos 
     candidatos surgem
     a todo momento.`
 
-    const texto2 = `A evolução da sua empresa não se dá apenas financeiramente, 
+  const texto2 = `A evolução da sua empresa não se dá apenas financeiramente, 
     mas sua imagem também impacta
     já quem é visto de forma agradável é sempre lembrado.`
 
-    const texto3 = `Sinta-se livre para disponibilizar novas vaga, 
+  const texto3 = `Sinta-se livre para disponibilizar novas vaga, 
     entretanto faça uso do sistema de recomendação
     já que ele está aqui apenas para ajudar.`
 
-    return (
-        <>
-            <div className="h-75vh flex justify-center items-center  aboutcontainer overflow-hidden ">
-                <img src="https://i.postimg.cc/3NqbdRXY/Andrew.png" alt="" className="flex " />
-                <div className="bg-gray-900 w-4/6 h-5/6 rounded-64px flex aboutextcontainer">
-                    <div className="content w-full h-full flex flex-col items-center  gap-8 py-20">
-                        <div className="flex flex-col items-center ">
-                            <h1 className="text-3xl font-bold uppercase text-white pt-2 text-center">{userProfile.name}</h1>
-                            <h2 className="text-2xl font-medium text-white pb-8 text-center ">O que podemos fazer hoje?</h2>
-                        </div>
-                        <div className="flex flex-wrap w-full justify-center px-4 md:px-16 gap-8 pb-8">
-  {/* Item 1 */}
-  <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
-    <FaClipboardUser className="text-white text-3xl" />
-    <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full">
-      {texto1}
-    </p>
-  </div>
+  return (
+    <>
+      <div className="h-75vh flex justify-center items-center  aboutcontainer overflow-hidden ">
+        <img src="https://i.postimg.cc/3NqbdRXY/Andrew.png" alt="" className="flex " />
+        <div className="bg-gray-900 w-4/6 h-5/6 rounded-64px flex aboutextcontainer">
+          <div className="content w-full h-full flex flex-col items-center  gap-8 py-20">
+            <div className="flex flex-col items-center ">
+              <h1 className="text-3xl font-bold uppercase text-white pt-2 text-center">{userProfile.name}</h1>
+              <h2 className="text-2xl font-medium text-white pb-8 text-center ">O que podemos fazer hoje?</h2>
+            </div>
+            <div className="flex flex-wrap w-full justify-center px-4 md:px-16 gap-8 pb-8">
+              {/* Item 1 */}
+              <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
+                <FaClipboardUser className="text-white text-3xl" />
+                <p className="text-justify  h-4/5 text-white bg-gray-800 p-3 rounded-lg font-normal w-full">
+                  {texto1}
+                </p>
+              </div>
 
-  {/* Item 2 */}
-  <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
-    <FaChartLine className="text-white text-3xl" />
-    <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full">
-      {texto2}
-    </p>
-  </div>
+              {/* Item 2 */}
+              <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
+                <FaChartLine className="text-white text-3xl" />
+                <p className="text-justify h-4/5 text-white bg-gray-800 p-3 rounded-lg font-normal w-full">
+                  {texto2}
+                </p>
+              </div>
 
-  {/* Item 3 */}
-  <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
-    <FaDove className="text-white text-3xl" />
-    <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full">
-      {texto3}
-    </p>
-  </div>
-</div>
-
-
-                    </div>
-                </div>
+              {/* Item 3 */}
+              <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
+                <FaDove className="text-white text-3xl" />
+                <p className="text-justify  h-4/5 text-white bg-gray-800 p-3 rounded-lg font-normal w-full">
+                  {texto3}
+                </p>
+              </div>
             </div>
 
-        </>
-    )
+
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
 
 }
 
--- Src/Apresentation/Web/src/Pages/Home/About.jsx
+++ Src/Apresentation/Web/src/Pages/Home/About.jsx
@@ -5,50 +5,50 @@ import { FaCircleInfo } from "react-icons/fa6";
 
 const About = () => {
 
-    return (
-        <>
-            <div className="h-75vh flex justify-center items-center  aboutcontainer overflow-hidden ">
-                <img src="https://i.postimg.cc/8ktnc47C/Girl-About.png" alt="" className="flex " />
-                <div className="bg-gray-900 w-4/6 h-5/6 rounded-64px flex aboutextcontainer">
-                    <div className="content w-full h-full flex flex-col items-center  gap-8 py-20">
-                        <div className="flex flex-col items-center ">
-                            <h1 className="text-3xl font-bold uppercase text-white pt-2 text-center">Quem Somos</h1>
-                            <h2 className="text-2xl font-medium text-white pb-8 text-center ">O que fazemos, como e o porquê.</h2>
-                        </div>
-
-                        <div className="flex flex-wrap w-full justify-center px-4 md:px-16 gap-8 pb-8">
-  {/* Item 1 */}
-  <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
-    <FaClipboardUser className="text-white text-3xl" />
-    <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full h-48 overflow-y-auto">
-      Fazemos a integração entre as empresas e as pessoas com deficiência (PCD) de forma ampla, em que a funcionalidade de nossa plataforma é projetada para o maior uso empresarial.
-    </p>
-  </div>
-
-  {/* Item 2 */}
-  <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
-    <FaChartLine className="text-white text-3xl" />
-    <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full h-48 overflow-y-auto">
-      Simplificamos o recrutamento de pessoas com deficiência (PCD), através de um sistema de vagas e de encontro de candidatos (Matchs) tornando esse processo mais prático possível.
-    </p>
-  </div>
-
-  {/* Item 3 */}
-  <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
-    <FaDove className="text-white text-3xl" />
-    <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full h-48 overflow-y-auto">
-      Acreditamos que iniciativas através da tecnologia possa trazer uma melhora significativa a vida de pessoas marginalizadas socialmente como em nosso caso os PCD.
-    </p>
-  </div>
-</div>
-
-
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="h-75vh flex justify-center items-center  aboutcontainer overflow-hidden ">
+        <img src="https://i.postimg.cc/8ktnc47C/Girl-About.png" alt="" className="flex " />
+        <div className="bg-gray-900 w-4/6 h-5/6 rounded-64px flex aboutextcontainer">
+          <div className="content w-full h-full flex flex-col items-center  gap-8 py-20">
+            <div className="flex flex-col items-center ">
+              <h1 className="text-3xl font-bold uppercase text-white pt-2 text-center">Quem Somos</h1>
+              <h2 className="text-2xl font-medium text-white pb-8 text-center ">O que fazemos, como e o porquê.</h2>
             </div>
 
-        </>
-    )
+            <div className="flex w-full  justify-center px-16 gap-8 pb-8  itemscontentsd">
+              {/* Item 1 */}
+              <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
+                <FaClipboardUser className="text-white text-3xl" />
+                <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full h-4/5 overflow-y-auto">
+                  Fazemos a integração entre as empresas e as pessoas com deficiência (PCD) de forma ampla, em que a funcionalidade de nossa plataforma é projetada para o maior uso empresarial.
+                </p>
+              </div>
+
+              {/* Item 2 */}
+              <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
+                <FaChartLine className="text-white text-3xl" />
+                <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full h-4/5 overflow-y-auto">
+                  Simplificamos o recrutamento de pessoas com deficiência (PCD), através de um sistema de vagas e de encontro de candidatos (Matchs) tornando esse processo mais prático possível.
+                </p>
+              </div>
+
+              {/* Item 3 */}
+              <div className="w-full md:w-1/3 flex flex-col items-center gap-4">
+                <FaDove className="text-white text-3xl" />
+                <p className="text-justify text-white bg-gray-800 p-3 rounded-lg font-normal w-full h-4/5 overflow-y-auto">
+                  Acreditamos que iniciativas através da tecnologia possa trazer uma melhora significativa a vida de pessoas marginalizadas socialmente como em nosso caso os PCD.
+                </p>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
 
 }
 
